27f54e7393cce117d932e18ad3abd1aca381e18b
mike@mikebarb.net <mike@mikebarb.net>
2026-06-09 22:32:20 +1000

    Another go - adding advance mode to astro config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,7 +7,9 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: 'advanced', // <--- ADD THIS: Forces a single _worker.js output
+  }),
   integrations: [svelte()],
 
   vite: {
