e27a86acdd8f61205f534fc3f19d76cdffc7c706
mike@mikebarb.net <mike@mikebarb.net>
2026-06-10 17:42:28 +1000

    Cloudflate Pages configuration changes. Now properly on pages.

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,19 +2,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  //output: 'static', // Astro generates pure HTML
-  adapter: cloudflare({
-    mode: 'directory', // THIS forces the single _worker.js file
-  }),
+  output: 'static',
+  trailingSlash: 'always',   // fixes Cloudflare sub‑page 403/404
   integrations: [svelte()],
-
   vite: {
     plugins: [tailwindcss()]
   }
 });
-
